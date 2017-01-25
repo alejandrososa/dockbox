2b9019dbe7093b6ca21d40463bb294ea377c9ce1
Alejandro Sosa <alejandro.sosa@advertising-go.es>
2017-01-25 18:11:32 +0100

    Fix php y apache

--- web/index.php
+++ web/index.php
@@ -5,4 +5,11 @@
  * Fecha: 24/1/17 - 21:43
  */
 
-echo 'Hola mundo';
+// Muestra toda la información, por defecto INFO_ALL
+phpinfo();
+
+// Muestra solamente la información de los módulos.
+// phpinfo(8) hace exactamente lo mismo.
+phpinfo(INFO_MODULES);
+
+?>
